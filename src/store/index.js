7635a65907240506d20bc15c36dd15d2b8fe0273
Enkot <taras.batenkov@gmail.com>
2021-11-14 21:53:38 +0200

    added classes to safelist

--- src/store/index.js
+++ src/store/index.js
@@ -17,14 +17,14 @@ export default new Vuex.Store({
       sortOption: 'newest',
     },
     typeColors: {
-      GQL: 'pink-500', // bg-pink-500 text-pink-500
-      QUERY: 'green-500', // bg-purple-500 text-purple-500
-      MUTATION: 'purple-500', // bg-indigo-500 text-indigo-500
-      GET: 'blue-500', // bg-blue-500 text-blue-500
-      POST: 'indigo-500', // bg-purple-500 text-purple-500
-      PUT: 'indigo-500', // bg-purple-500 text-purple-500
-      PATCH: 'indigo-500', // bg-purple-500 text-purple-500
-      DELETE: 'rose-500', // bg-rose-500 text-rose-500
+      GQL: 'pink-500', // bg-pink-500 text-pink-500 group-hover:bg-pink-500
+      QUERY: 'green-500', // bg-green-500 text-green-500 group-hover:bg-green-500
+      MUTATION: 'purple-500', // bg-purple-500 text-purple-500 group-hover:bg-purple-500
+      GET: 'blue-500', // bg-blue-500 text-blue-500 group-hover:bg-blue-500
+      POST: 'indigo-500', // bg-indigo-500 text-indigo-500 group-hover:bg-indigo-500
+      PUT: 'indigo-500', // bg-indigo-500 text-indigo-500 group-hover:bg-indigo-500
+      PATCH: 'indigo-500', // bg-indigo-500 text-indigo-500 group-hover:bg-indigo-500
+      DELETE: 'rose-500', // bg-rose-500 text-rose-500 group-hover:bg-rose-500
     },
   },
   mutations: {
